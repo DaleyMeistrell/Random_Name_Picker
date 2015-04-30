8496c7d12287928a12242f8dad1ba29b8d2fa504
Daley Meistrell <dstrell14@gmail.com>
2015-04-30 16:39:36 -0700

    built javascript logic

---
+++ random_name_picker.js
@@ -0,0 +1,15 @@
+var bucket_list = ['Greece', 'France', 'Nepal',
+ 'Australia', 'Thailand', 'Switzerland', 'Panama',
+  'Ireland', 'Spain', 'Portugal'];
+
+  console.log(bucket_list[2]);
+  console.log(bucket_list[5]);
+  console.log(bucket_list);
+
+
+var i = 0;
+    function eachDestination(){
+      console.log(bucket_list[i]);
+      i ++; 
+    }
+    eachDestination();
