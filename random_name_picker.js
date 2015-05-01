1c78d82a6f17f0ef32d21ff7cd77584c94488577
Daley Meistrell <dstrell14@gmail.com>
2015-04-30 17:19:01 -0700

    javascript displaying correctly in console

--- random_name_picker.js
+++ random_name_picker.js
@@ -1,15 +1,29 @@
-var bucket_list = ['Greece', 'France', 'Nepal',
- 'Australia', 'Thailand', 'Switzerland', 'Panama',
-  'Ireland', 'Spain', 'Portugal'];
+ var bucket_list = ['Greece', 'France', 'Nepal',
+  'Australia', 'Thailand', 'Switzerland', 'Panama',
+   'Ireland', 'Spain', 'Portugal'];
 
-  console.log(bucket_list[2]);
-  console.log(bucket_list[5]);
-  console.log(bucket_list);
+   /*console.log(bucket_list[2]);
+   console.log(bucket_list[5]);
+   console.log(bucket_list);*/
 
 
-var i = 0;
-    function eachDestination(){
-      console.log(bucket_list[i]);
-      i ++; 
-    }
-    eachDestination();
+ /*var i = 0;
+     function eachDestination(){
+       console.log(bucket_list[i]);
+       i++; 
+     }
+    eachDestination();*/
+function eachDestination() { 
+var pick = Math.floor(Math.random() * bucket_list.length)
+var destinationPicker = bucket_list[pick];
+console.log(destinationPicker);
+}
+
+/*var pick = bucket_list[Math.floor(Math.random()*bucket_list.length)];
+
+var destinationPicker = bucket_list[Math.floor(Math.random() * bucket_list.length)];
+console.log(destinationPicker);
+
+/*var eachDestination = function() {
+	console.log(eachDestination)
+};*/
